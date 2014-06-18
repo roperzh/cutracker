11c485d3525d00182bbd2e9fdc5196ed76fc1c9d
Roberto Dip <dip.jesusr@gmail.com>
2014-06-17 21:59:50 -0300

    Implement the user model
    - Validates name, surname, password and email fields
    - Implement the shield protocol for validations

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User
+  include Mongoid::Document
+  include Shield::Model
+
+  # Field definitions
+  field :name,             type: String
+  field :surname,          type: String
+  field :email,            type: String
+  field :crypted_password, type: String
+
+  # Validations
+  validates :name, :surname, :email, :crypted_password, presence: true
+  validates :email, uniqueness: true
+  validates :email, format: {
+    with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  }
+
+  # Associations
+  has_and_belongs_to_many :projects
+
+  def self.[](id)
+    find(id)
+  end
+
+  def self.fetch(email)
+    where(email: email).first
+  end
+end
