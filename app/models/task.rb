4d6f160ebdbd314250ab6a19ee57b7affe8f881f
Roberto Dip <dip.jesusr@gmail.com>
2014-07-10 00:14:26 -0300

    Add default values and validations to the task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,12 +2,16 @@ class Task
   include Mongoid::Document
   include Mongoid::TagsArentHard
 
+  # Field definitions
   field :name,        type: String
-  field :duration,    type: String
+  field :duration,    type: String, default: "00:00"
   field :description, type: String
-  field :status,      type: Integer
+  field :status,      type: Integer, default: 0
 
   taggable_with :tags
+
+  # Validations
+  validates :name, :status, presence: true
 
   # Associations
   belongs_to :user
