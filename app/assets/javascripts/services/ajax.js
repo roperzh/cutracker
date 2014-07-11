13485423c83c2d519a54a83c7fc6e47d167d1c17
Roberto Dip <dip.jesusr@gmail.com>
2014-07-11 18:26:53 -0300

    Update the ajax service, allowing more flexible POST requests

--- app/assets/javascripts/services/ajax.js
+++ app/assets/javascripts/services/ajax.js
@@ -15,6 +15,14 @@ Ajax = {
 
   post: function(options) {
     options.method = "POST";
+
+    /* If the data isn't a FormData object, lets encode it */
+    if(!options.data.toString().match("FormData")) {
+      options.headers = options.headers || {};
+      options.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+      options.data = formurlencoded.encode(options.data);
+    }
+
     this.request(options);
   },
 
@@ -22,14 +30,21 @@ Ajax = {
     var request = new XMLHttpRequest();
 
     request.open(options.method, options.url);
+    this.setHeaders(request, options.headers);
     this.setCallbacks(request, options);
     request.send(options.data);
+  },
+
+  setHeaders: function(request, headers) {
+    for(var headerName in headers) {
+      request.setRequestHeader(headerName, headers[headerName]);
+    }
   },
 
   setCallbacks: function(request, options) {
     request.onload = function() {
       if (request.status >= 200 && request.status < 400) {
-        options.success(request);
+        options.success(request);task
       } else {
         options.error(request);
       }
