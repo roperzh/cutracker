17f4e44ba194f9f6f42e2e82adc5d72e319c0958
Roberto Dip <dip.jesusr@gmail.com>
2014-08-05 00:50:05 -0300

    Add the tabbed-container behavior

---
+++ app/assets/javascripts/behaviors/tabbed-container/tabbed-container.js
@@ -0,0 +1,29 @@
+// -------------------------------------------
+//   Tabbed Container
+//   > Container with multiple tabs
+// ------------------------------------------
+
+Ui.TabbedContainer = Essential.Behavior.extend({
+  events: {
+    "click .tab-header": "switchTab"
+  },
+
+  switchTab: function(e) {
+    e.preventDefault();
+
+    var selectedHeader = e.currentTarget,
+      tabName = selectedHeader.getAttribute("data-content"),
+      selectedContent = this.el.querySelectorAll("[data-name=" + tabName + "]")[0];
+
+    var activeItems = this.el.getElementsByClassName("active"),
+      itemsCount = activeItems.length;
+      i = -1;
+
+    while(++i < itemsCount) {
+      activeItems[0].classList.remove("active");
+    }
+
+    selectedHeader.classList.add("active");
+    selectedContent.classList.add("active");
+  }
+});
